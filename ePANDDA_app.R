02a181584842a542ccfe5ab3199e0b30b46363f7
Erika Ito <etito@wisc.edu>
2017-05-10 11:15:05 -0500

    Major update

--- ePANDDA_app.R
+++ ePANDDA_app.R
@@ -1,6 +1,6 @@
 Start<-print(Sys.time())
 
-# Install libraries if necessary and load them into the environment
+######################################### Load Required Libraries ###########################################
 if (require("RCurl",warn.conflicts=FALSE)==FALSE) {
     install.packages("RCurl",repos="http://cran.cnr.berkeley.edu/");
     library("RCurl");
@@ -34,12 +34,35 @@ if (length(CommandArgument)==0) {
     Cluster<-makeCluster(as.numeric(CommandArgument[1]))
     }
 
+#############################################################################################################
+######################################### DATA DOWNLOAD, EPANDDA ############################################
+#############################################################################################################
+# No functions at this time.
+
+############################################ Download Datasets from API  ####################################
+
 print(paste("download PBDB refs",Sys.time()))
 
-# download all references from PBDB
-RefsURL<-"https://paleobiodb.org/data1.2/colls/refs.csv?all_records"
-GotURL<-getURL(RefsURL)
+# Increase the timeout option to allow for larger data downloads
+options(timeout=300)
+
+# Download references from the Paleobiology Database through the API
+GotURL<-RCurl::getURL("https://paleobiodb.org/data1.2/colls/refs.csv?all_records")
 PBDBRefs<-read.csv(text=GotURL,header=TRUE)
+
+# Pull out only the needed columns and rename them to match GDDRefs
+PBDBRefs<-PBDBRefs[,c("reference_no","author1last","pubyr","reftitle","pubtitle")]
+colnames(PBDBRefs)<-c("pbdb_no","pbdb_author","pbdb_year","pbdb_title","pbdb_pubtitle")
+
+# Change data types of PBDBRefs to appropriate types
+PBDBRefs[,"pbdb_no"]<-as.numeric(as.character(PBDBRefs[,"pbdb_no"]))
+PBDBRefs[,"pbdb_author"]<-as.character(PBDBRefs[,"pbdb_author"])
+PBDBRefs[,"pbdb_year"]<-as.numeric(as.character(PBDBRefs[,"pbdb_year"]))
+PBDBRefs[,"pbdb_title"]<-as.character(PBDBRefs[,"pbdb_title"])
+PBDBRefs[,"pbdb_pubtitle"]<-as.character(PBDBRefs[,"pbdb_pubtitle"])
+
+# Remove PBDB Refs with no title
+PBDBRefs<-subset(PBDBRefs,nchar(PBDBRefs[,"pbdb_title"])>2)
 
 # Find the current directory
 CurrentDirectory<-getwd()
@@ -49,77 +72,47 @@ print(paste("download DD refs",Sys.time()))
 # Move othe input folder
 setwd(paste(CurrentDirectory,"/input",sep=""))
 # Load in the input.bibjson file
-DDRefs<-fromJSON("input.bibjson") # if testing: "~/Documents/DeepDive/ePANDDA/EPANDDA_app-master/input/input.bibjson"
+GDDRefs<-fromJSON("input.bibjson") # if testing: "~/Documents/DeepDive/ePANDDA/EPANDDA_app-master/input/input.bibjson"
 
-############### DEBUG STEP ##
-print("debug stage 1")
-print(head(DDRefs))
+# Extract authors, docid, year, title, journal, and publisher information from the BibJson List into vectors
+gdd_id<-parSapply(Cluster,GDDRefs,function(x) x[["_gddid"]])
+gdd_author<-parSapply(Cluster,GDDRefs,function(x) paste(unlist(x[["author"]]),collapse=" "))
+gdd_year<-parSapply(Cluster,GDDRefs,function(x) x[["year"]])
+gdd_title<-parSapply(Cluster,GDDRefs,function(x) x[["title"]])
+gdd_pubtitle<-parSapply(Cluster,GDDRefs,function(x) x[["journal"]])
+gdd_publisher<-parSapply(Cluster,GDDRefs,function(x) x[["publisher"]])
 
-print(paste("make DDRefs columns",Sys.time()))
-
-print(paste("make a column of DD reference number",Sys.time()))
-gdd_id<-parSapply(Cluster,DDRefs,function(x) x[["_gddid"]])
-print(paste("make a vector of DD authors",Sys.time()))
-gdd_author<-parSapply(Cluster,DDRefs,function(x) paste(unlist(x[["author"]]),collapse=" "))
-print(paste("make a vector of DD publication years",Sys.time()))
-gdd_year<-parSapply(Cluster,DDRefs,function(x) x[["year"]])
-print(paste("make a vector of DD ref titles",Sys.time()))
-gdd_title<-parSapply(Cluster,DDRefs,function(x) x[["title"]])
-print(paste("make a column of DD journal names",Sys.time())) 
-gdd_pubtitle<-parSapply(Cluster,DDRefs,function(x) x[["journal"]])
-print(paste("make a column of DD publishers",Sys.time())) 
-gdd_publisher<-parSapply(Cluster,DDRefs,function(x) x[["publisher"]])
-
-# Bind DDRefs columns of interest
-DDRefs<-cbind(gdd_id,gdd_author,gdd_year,gdd_title,gdd_pubtitle, gdd_publisher)
+# Create identically formatted data.frames for geodeepdive and pbdb references (overwrite GDDRefs)
+GDDRefs<-as.data.frame(cbind(gdd_id,gdd_author,gdd_year,gdd_title,gdd_pubtitle, gdd_publisher),stringsAsFactors=FALSE)
     
+# Change data types of DDRefs to appropriate types
+GDDRefs[,"gdd_id"]<-as.character(GDDRefs[,"gdd_id"])
+GDDRefs[,"gdd_author"]<-as.character(GDDRefs[,"gdd_author"])
+GDDRefs[,"gdd_year"]<-as.numeric(as.character(GDDRefs[,"gdd_year"]))
+GDDRefs[,"gdd_title"]<-as.character(GDDRefs[,"gdd_title"])
+GDDRefs[,"gdd_pubtitle"]<-as.character(GDDRefs[,"gdd_pubtitle"])
+GDDRefs[,"gdd_publisher"]<-as.character(GDDRefs[,"gdd_publisher"])
+                         
 # Update DDRefs[,"gdd_pubtitle"] to usgs bulletin where appropriate
 print(paste("usgs bulletin update",Sys.time()))   
-USGS_Bulletin<-which(DDRefs[,"gdd_pubtitle"]=="Bulletin"&DDRefs[,"gdd_publisher"]=="USGS")
-DDRefs[USGS_Bulletin,"gdd_pubtitle"]<-"usgs bulletin"
+USGS_Bulletin<-which(GDDRefs[,"gdd_pubtitle"]=="Bulletin"&GDDRefs[,"gdd_publisher"]=="USGS")
+GDDRefs[USGS_Bulletin,"gdd_pubtitle"]<-"usgs bulletin"
 # update and overwrite gdd_pubtitle
-gdd_pubtitle<-DDRefs[,"gdd_pubtitle"]
-    
-# create identically formatted matrices for geodeepdive and pbdb references (overwrite DDRefs)
-DDRefs<-cbind(gdd_id,gdd_author,gdd_year,gdd_title,gdd_pubtitle, gdd_publisher)
-PBDBRefs<-cbind(PBDBRefs[c("reference_no","author1last","pubyr","reftitle","pubtitle")])
-colnames(PBDBRefs)<-c("pbdb_no","pbdb_author","pbdb_year","pbdb_title","pbdb_pubtitle")
+gdd_pubtitle<-GDDRefs[,"gdd_pubtitle"]
 
-# convert matrices to dataframes
-DDRefs<-as.data.frame(DDRefs)
-PBDBRefs<-as.data.frame(PBDBRefs)
-    
-############### DEBUG STEP ##
-print("debug stage 2")
-print(head(DDRefs))
-print(head(PBDBRefs))
-
-# Change data types of DDRefs to appropriate types
-DDRefs[,"gdd_id"]<-as.character(DDRefs[,"gdd_id"])
-DDRefs[,"gdd_author"]<-as.character(DDRefs[,"gdd_author"])
-DDRefs[,"gdd_year"]<-as.numeric(as.character(DDRefs[,"gdd_year"]))
-DDRefs[,"gdd_title"]<-as.character(DDRefs[,"gdd_title"])
-DDRefs[,"gdd_pubtitle"]<-as.character(DDRefs[,"gdd_pubtitle"])
-# Change data types of PBDBRefs to appropriate types
-PBDBRefs[,"pbdb_no"]<-as.numeric(as.character(PBDBRefs[,"pbdb_no"]))
-PBDBRefs[,"pbdb_author"]<-as.character(PBDBRefs[,"pbdb_author"])
-PBDBRefs[,"pbdb_year"]<-as.numeric(as.character(PBDBRefs[,"pbdb_year"]))
-PBDBRefs[,"pbdb_title"]<-as.character(PBDBRefs[,"pbdb_title"])
-PBDBRefs[,"pbdb_pubtitle"]<-as.character(PBDBRefs[,"pbdb_pubtitle"])
- 
-# Convert the title and pubtitle to all caps, because stringsim cannot distinguish between cases
+# Convert the title and pubtitle to all lower case, because stringsim, unlike grep, cannot distinguish between cases
 PBDBRefs[,"pbdb_title"]<-tolower(PBDBRefs[,"pbdb_title"])
 PBDBRefs[,"pbdb_pubtitle"]<-tolower(PBDBRefs[,"pbdb_pubtitle"])
-DDRefs[,"gdd_title"]<-tolower(DDRefs[,"gdd_title"])
-DDRefs[,"gdd_pubtitle"]<-tolower(DDRefs[,"gdd_pubtitle"])
-
-# RECORD STATS 
+GDDRefs[,"gdd_title"]<-tolower(GDDRefs[,"gdd_title"])
+GDDRefs[,"gdd_pubtitle"]<-tolower(GDDRefs[,"gdd_pubtitle"])
+                                                 
+# Record stats 
 # Record the initial number of PBDB documents
 PBDBDocs<-dim(PBDBRefs)[1]
 # Record the initial number of GeoDeepDive documents
-DDDocs<-dim(DDRefs)[1]    
-    
-# Try to harmonize journal titles between PBDB and GDD
+GDDDocs<-dim(GDDRefs)[1]   
+                         
+############################### HARMONIZE JOURNAL TITLES BETWEEN PBDB AND GDD ################################
 print(paste("harmonize publication titles between pbdb and gdd",Sys.time()))
 
 # replace all versions of "United States Geological Survey" in PBDB with "usgs" 
@@ -139,73 +132,89 @@ PBDBRefs[geobios,"pbdb_pubtitle"]<-"geobios"
 canadian_journal<-which(PBDBRefs[,"pbdb_pubtitle"]=="canadian journal of earth science"|
 PBDBRefs[,"pbdb_pubtitle"]=="canadian journal earth science")
 # Replace titles to match GeoDeepDive
-PBDBRefs[canadian_journal,"pbdb_pubtitle"]<-"canadian journal of earth sciences"  
-
-############### DEBUG STEP ##
-print("debug stage 3")
-print(head(DDRefs))
-print(head(PBDBRefs))    
+PBDBRefs[canadian_journal,"pbdb_pubtitle"]<-"canadian journal of earth sciences"    
     
-### Phase 2: A MATCHING FUNCTION IS BORN
+#############################################################################################################
+########################################## MATCH TITLES, EPANDDA ############################################
+#############################################################################################################
+# Find the best title stringsim for each PBDB ref in GDD
 matchTitle<-function(x,y) {
-    Similarity<-stringsim(x,y)
-    max_title<-max(Similarity)
-    max_no<-which.max(Similarity)
-    return(c(max_no,max_title))
+    Similarity<-stringdist::stringsim(x,y)
+    MaxTitle<-max(Similarity)
+    MaxIndex<-which.max(Similarity)
+    return(c(MaxIndex,MaxTitle))
     }
-    
+
+############################################ Initial Title Match Script #####################################       
 # Status update
 print(paste("perform title matches",Sys.time()))   
-    
+
+# Export the functions to the cluster
+clusterExport(cl=Cluster,varlist=c("matchTitle","stringsim"))                         
+                         
 # Find the best title stringsim for each PBDB ref in GDD
-clusterExport(cl=Cluster,varlist=c("matchTitle","stringsim"))
-TitleSimilarity<-parSapply(Cluster,PBDBRefs[,"pbdb_title"],matchTitle,DDRefs[,"gdd_title"])
+TitleSimilarity<-parSapply(Cluster,PBDBRefs[,"pbdb_title"],matchTitle,GDDRefs[,"gdd_title"])
 # Reshape the Title Similarity Output
 TitleSimilarity<-as.data.frame(t(unname(TitleSimilarity)))
-    
-# Bind Title Similarity with pbdb_no
+                         
+# Bind Title Similarity by pbdb_no
 InitialMatches<-cbind(PBDBRefs[,"pbdb_no"],TitleSimilarity)
-InitialMatches[,"V1"]<-DDRefs[InitialMatches[,"V1"],"gdd_id"]
+InitialMatches[,"V1"]<-GDDRefs[InitialMatches[,"V1"],"gdd_id"]
 colnames(InitialMatches)<-c("pbdb_no","gdd_id","title_sim")
 
 # Merge initial matches, pbdb refs, and gdd refs
-InitialMatches<-merge(InitialMatches,DDRefs,by="gdd_id",all.x=TRUE)
+InitialMatches<-merge(InitialMatches,GDDRefs,by="gdd_id",all.x=TRUE)
 InitialMatches<-merge(InitialMatches,PBDBRefs,by="pbdb_no",all.x=TRUE)
-    
+                        
 # Status update
 print(paste("finish title matches",Sys.time()))
 
-############### DEBUG STEP ##
-print("debug stage 4")
-print(head(InitialMatches))    
-    
-### Phase 3: Matching additional similarity information
-print(paste("perform additional matches",Sys.time()))
-    
-# A function for matching PBDB and DDRefs
+#############################################################################################################
+########################################## MATCH FIELDS, EPANDDA ############################################
+#############################################################################################################                        
+# A function for matching additional bibliographic fields between the best and worst match
 matchAdditional<-function(InitialMatches) {
-    # Pub year match
+    # Whether the publication year is identical
     Year<-InitialMatches["pbdb_year"]==InitialMatches["gdd_year"]
-    # Journal Similarity
+    # The similarity of the journal names
     Journal<-stringsim(InitialMatches["pbdb_pubtitle"],InitialMatches["gdd_pubtitle"])
-    # Author present
+    # Whether the first author's surname is present in the GDD bibliography
     Author<-grepl(InitialMatches["pbdb_author"],InitialMatches["gdd_author"],perl=TRUE,ignore.case=TRUE)
     # Return output     
     FinalOutput<-setNames(c(InitialMatches["pbdb_no"],InitialMatches["gdd_id"],InitialMatches["title_sim"],Author,Year,Journal),c("pbdb_no","gdd_id","title_sim","author_in","year_match","pubtitle_sim"))
     return(FinalOutput)
-    }
+    }                   
+######################################### Match Additional Fields Script ####################################  
+print(paste("perform additional matches",Sys.time()))
+
+# Reset the data types; columns are sometimes coerced to the incorrect data type for unknown reasons
+InitialMatches[,"pbdb_no"]<-as.numeric(InitialMatches[,"pbdb_no"])
+InitialMatches[,"gdd_id"]<-as.character(InitialMatches[,"gdd_id"])
+InitialMatches[,"title_sim"]<-as.numeric(InitialMatches[,"title_sim"])
+InitialMatches[,"gdd_author"]<-as.character(InitialMatches[,"gdd_author"])
+InitialMatches[,"gdd_year"]<-as.numeric(InitialMatches[,"gdd_year"])
+InitialMatches[,"gdd_title"]<-as.character(InitialMatches[,"gdd_title"])
+InitialMatches[,"gdd_pubtitle"]<-as.character(InitialMatches[,"gdd_pubtitle"]) 
+InitialMatches[,"gdd_publisher"]<-as.character(InitialMatches[,"gdd_publisher"]) # This is where the break was happening
+InitialMatches[,"pbdb_author"]<-as.character(InitialMatches[,"pbdb_author"])
+InitialMatches[,"pbdb_year"]<-as.numeric(InitialMatches[,"pbdb_year"])
+InitialMatches[,"pbdb_title"]<-as.character(InitialMatches[,"pbdb_title"])
+InitialMatches[,"pbdb_pubtitle"]<-as.character(InitialMatches[,"pbdb_pubtitle"])
 
 # export matchAdditional to the cluster
-clusterExport(cl=Cluster,varlist=c("matchAdditional"))                           
+clusterExport(cl=Cluster,varlist=c("matchAdditional"))    
+                         
+# Perform the additional matches
 MatchReferences<-parApply(Cluster, InitialMatches, 1, matchAdditional)
+                         
 # Stop the Cluser
 stopCluster(Cluster)
+                         
 # Reformat MatchReferences
-MatchReferences<-as.data.frame(t(MatchReferences))
-  
+MatchReferences<-as.data.frame(t(MatchReferences),stringsAsFactors=FALSE)
     
 print(paste("organize stats",Sys.time()))    
-# RECORD STATS
+# Record Stats
 # Create a vector of the title_sim column of MatchReferences
 TitleSim<-as.numeric(as.character(MatchReferences[,"title_sim"]))
 # Create a table showing title_sim values rounded to the nearest hundredth    
@@ -214,11 +223,11 @@ RoundedTitleSim<-paste(names(TitleSimTable), collapse=" ")
 Refs<-paste(TitleSimTable, collapse=" ")
 
 # Create stat descriptions
-Descriptions<-c("Date","Initial number of PBDBRefs","Initial number of DDRefs","Rounded title similarities","Number of references")
+Descriptions<-c("Date","Initial number of PBDBRefs","Initial number of GDDRefs","Rounded title similarities","Number of references")
 # Create date and time record for stats file
 Date<-as.character(Sys.time())
 # Bind stats
-Stats<-rbind(Date,PBDBDocs,DDDocs,RoundedTitleSim,Refs)
+Stats<-rbind(Date,PBDBDocs,GDDDocs,RoundedTitleSim,Refs)
 Stats<-as.data.frame(cbind(Stats,Descriptions),row.names=FALSE)
 colnames(Stats)<-c("Stats","Descriptions") 
     
@@ -236,9 +245,8 @@ setwd(paste(CurrentDirectory,"/output",sep=""))
 unlink("*")
 
 # Write the output
-saveRDS(MatchReferences, "MatchReferences.rds")
-saveRDS(DDRefs, "DDRefs.rds")
-saveRDS(PBDBRefs, "PBDBRefs.rds")
+write.csv(GDDRefs, "DDRefs.csv")
+write.csv(PBDBRefs, "PBDBRefs.csv")
 write.csv(MatchReferences, "MatchReferences.csv")
 write.csv(Stats,"Stats.csv",row.names=FALSE)
 
